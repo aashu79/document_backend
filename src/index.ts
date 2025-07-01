bcfc6ba72831e9cbe0dd7aaaf3f6eaba01de59b8
aashu79 <aashusah435@gmail.com>
2025-07-01 20:47:16 +0545

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
