16f4adc921d27a4f8bf72931bbc7d5eac09a95ea
aashu79 <aashusah435@gmail.com>
2025-07-03 22:07:41 +0545

    added the missing document_type route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route";
+import documentTypeRoutes from "./routes/document.route";
 import path from "path";
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/document-types", documentTypeRoutes);
 
 // Server start
 app.listen(PORT, () => {
